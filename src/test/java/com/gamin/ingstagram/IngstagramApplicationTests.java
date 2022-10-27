7434a606af2680d51571d9856e8c9543cdf4d614
Minwoo Chase <c5287@nate.com>
2022-10-27 09:44:13 +0900

    Initial commit

---
+++ src/test/java/com/gamin/ingstagram/IngstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamin.ingstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
